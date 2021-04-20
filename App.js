5f778d56683e0ca7c11d55981c11bd4b4e588a98
TSOUMOU-BITA <72856494+TSOUMOU-BITA@users.noreply.github.com>
2021-04-21 00:33:13 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
+
+import ToDoList from './TodDo/ToDolist ';
+
+class App extends Component{
+  rendet(){
+    return {
+      <TodoList />
+    };
+  }
+}
+
+export default App;
