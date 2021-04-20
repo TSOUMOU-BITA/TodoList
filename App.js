4de9d35e9dba11b14cc1ea7de4536248cc55c2e9
TSOUMOU-BITA <72856494+TSOUMOU-BITA@users.noreply.github.com>
2021-04-21 01:38:38 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '.App.css';
+import ToDoList from './TodDo/ToDolist ';
+
+class App extends Component{
+    render(){
+    return {
+        <TodoList />
+    };
+    }
+}
+
+export default App;
